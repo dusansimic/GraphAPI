25326ef0a77903fa1ec35f8be67ba92233af7e43
Dušan Simić <dusan.simic1810@gmail.com>
2023-10-31 00:31:31 +0100

    Add codes from first lab

--- lib/src/main/java/graphapi/Graph.java
+++ lib/src/main/java/graphapi/Graph.java
@@ -2,24 +2,49 @@ package graphapi;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Graph {
-  private class IntegerList extends ArrayList<Integer> {
+  private class IntegerSet extends HashSet<Integer> {
   }
 
-  private IntegerList[] neighbours;
+  private class Bag<T> extends HashSet<T> {
+    public T pick() {
+      if (isEmpty()) {
+        return null;
+      }
+
+      T t = iterator().next();
+      remove(t);
+      return t;
+    }
+  }
+
+  public class Pair<V, W> {
+    public final V v;
+    public final W w;
+
+    public Pair(V v, W w) {
+      this.v = v;
+      this.w = w;
+    }
+  }
+
+  private IntegerSet[] neighbours;
   private int edges;
 
   /**
    * Create a new graph with no vertices.
    */
   public Graph() {
-    neighbours = new IntegerList[0];
+    neighbours = new IntegerSet[0];
   }
 
   /**
    * Create a new graph with v vertices and no edges.
-   * 
+   *
    * @param v Number of vertices
    * @throws IllegalArgumentException if v is negative
    */
@@ -28,10 +53,10 @@ public class Graph {
       throw new IllegalArgumentException("Number of vertices must be non-negative");
     }
 
-    neighbours = new IntegerList[v];
+    neighbours = new IntegerSet[v];
 
     for (int i = 0; i < v; i++) {
-      neighbours[i] = new IntegerList();
+      neighbours[i] = new IntegerSet();
     }
 
     edges = 0;
@@ -39,7 +64,7 @@ public class Graph {
 
   /**
    * Create a new graph from input stream.
-   * 
+   *
    * @param in Input stream
    * @throws NumberFormatException if the file does not contain a valid integer
    * @throws IOException           if the file cannot be read
@@ -58,7 +83,7 @@ public class Graph {
 
   /**
    * Number of vertices in the graph.
-   * 
+   *
    * @return Number of vertices
    */
   public int V() {
@@ -67,7 +92,7 @@ public class Graph {
 
   /**
    * Number of edges in the graph.
-   * 
+   *
    * @return Number of edges
    */
   public int E() {
@@ -78,19 +103,19 @@ public class Graph {
    * Add a new vertex to the graph.
    */
   public void addVertex() {
-    IntegerList[] newNeighbours = new IntegerList[neighbours.length + 1];
+    IntegerSet[] newNeighbours = new IntegerSet[neighbours.length + 1];
 
     for (int i = 0; i < neighbours.length; i++) {
       newNeighbours[i] = neighbours[i];
     }
 
-    newNeighbours[neighbours.length] = new IntegerList();
+    newNeighbours[neighbours.length] = new IntegerSet();
     neighbours = newNeighbours;
   }
 
   /**
    * Remove a vertex from the graph.
-   * 
+   *
    * @param v Vertex to remove
    * @throws IllegalArgumentException if v is not present in the graph
    */
@@ -101,7 +126,7 @@ public class Graph {
 
     edges -= neighbours[v].size();
 
-    IntegerList[] newNeighbours = new IntegerList[neighbours.length - 1];
+    IntegerSet[] newNeighbours = new IntegerSet[neighbours.length - 1];
 
     for (int i = 0; i < v; i++) {
       newNeighbours[i] = neighbours[i];
@@ -113,14 +138,14 @@ public class Graph {
 
     neighbours = newNeighbours;
 
-    for (IntegerList neighs : neighbours) {
+    for (IntegerSet neighs : neighbours) {
       neighs.remove((Integer) v);
     }
   }
 
   /**
    * Add an edge between vertices v and w.
-   * 
+   *
    * @param v Vertex
    * @param w Vertex
    * @throws IllegalArgumentException if v or w is not present in the graph
@@ -137,7 +162,7 @@ public class Graph {
 
   /**
    * Remove an edge between vertices v and w.
-   * 
+   *
    * @param v Vertex
    * @param w Vertex
    * @throws IllegalArgumentException if v or w is not present in the graph
@@ -154,7 +179,7 @@ public class Graph {
 
   /**
    * Vertices adjacent to v.
-   * 
+   *
    * @param v Vertex
    * @return Iterable of vertices adjacent to v
    * @throws IllegalArgumentException if v is not present in the graph
@@ -169,7 +194,7 @@ public class Graph {
 
   /**
    * Degree of vertex v.
-   * 
+   *
    * @param v Vertex
    * @return Degree of vertex v
    * @throws IllegalArgumentException if v is not present in the graph
@@ -183,8 +208,225 @@ public class Graph {
   }
 
   /**
+   * Check if there is a path between two vertices.
+   *
+   * @param v Vertex
+   * @param w Vertex
+   * @return True if there is a path between v and w, false otherwise
+   */
+  public boolean hasPath(int v, int w) {
+    for (int x : component(v)) {
+      if (x == w) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Find the shortest path between vertex v and w.
+   *
+   * @param v Vertex
+   * @param w Vertex
+   * @return Iterable of vertices in the path
+   */
+  public Iterable<Integer> path(int v, int w) {
+    boolean[] marker = new boolean[neighbours.length];
+    int[] edgeTo = new int[neighbours.length];
+    int[] distTo = new int[neighbours.length];
+
+    distTo[v] = 0;
+
+    dfsMarker(v, v, marker, edgeTo, distTo);
+
+    if (!marker[w]) {
+      return null;
+    }
+
+    ArrayList<Integer> path = new ArrayList<>();
+
+    for (int x = w; x != v;) {
+      path.add(0, x);
+      x = edgeTo[x];
+    }
+
+    path.add(0, v);
+
+    return path;
+  }
+
+  /**
+   * Find all the components in the graph.
+   *
+   * @return Iterable of components
+   */
+  public Iterable<Iterable<Integer>> components() {
+    ArrayList<Iterable<Integer>> comps = new ArrayList<>();
+    Bag<Integer> bag = new Bag<>();
+    for (int i = 0; i < neighbours.length; i++) {
+      bag.add(i);
+    }
+
+    while (!bag.isEmpty()) {
+      int v = bag.pick();
+      Iterable<Integer> comp = component(v);
+
+      for (int w : comp) {
+        bag.remove(w);
+      }
+
+      comps.add(comp);
+    }
+
+    return comps;
+  }
+
+  /**
+   * Find the component which contains vertex v.
+   *
+   * @param v
+   * @return Iterable of vertices in the component
+   */
+  public Iterable<Integer> component(int v) {
+    if (v < 0 || v >= neighbours.length) {
+      throw new IllegalArgumentException("Vertices must be present in graph");
+    }
+
+    boolean[] marked = new boolean[neighbours.length];
+    int[] edgeTo = new int[neighbours.length];
+    int[] distTo = new int[neighbours.length];
+
+    distTo[v] = 0;
+
+    dfsMarker(v, v, marked, edgeTo, distTo);
+
+    HashSet<Integer> comp = new HashSet<>();
+    for (int i = 0; i < marked.length; i++) {
+      if (marked[i]) {
+        comp.add(i);
+      }
+    }
+
+    return comp;
+  }
+
+  /**
+   * Check if v is in a contour.
+   *
+   * @param v
+   * @return True if v is in a contour, false otherwise
+   */
+  public boolean hasContours(int v) {
+    if (v < 0 || v >= neighbours.length) {
+      throw new IllegalArgumentException("Vertices must be present in graph");
+    }
+
+    return dfsCountourFinder(v);
+  }
+
+  /**
+   * DFS implementation specificaly used for finding contours which contain
+   * vertex v.
+   *
+   * @param v Vertex
+   * @return True if v is in a contour, false otherwise
+   */
+  public boolean dfsCountourFinder(int v) {
+    LinkedList<Integer> s = new LinkedList<>();
+    boolean[] visited = new boolean[neighbours.length];
+    visited[v] = true;
+
+    for (int w : neighbours[v]) {
+      s.push(w);
+    }
+
+    while (!s.isEmpty()) {
+      int w = s.pop();
+      visited[w] = true;
+
+      for (int x : neighbours[w]) {
+        if (!visited[x] && neighbours[v].contains(x)) {
+          return true;
+        }
+
+        if (!visited[x]) {
+          s.push(x);
+        }
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Use dfs to go over all vertices and mark them.
+   *
+   * @param v      Current vertex
+   * @param w      Edge to vertex
+   * @param marked Array of marked vertices
+   * @param edgeTo Array of vertices that edge to the index vertex
+   * @param distTo Array of distances from vertex v
+   */
+  private void dfsMarker(int v, int w, boolean[] marked, int[] edgeTo, int[] distTo) {
+    if (marked[v]) {
+      return;
+    }
+
+    marked[v] = true;
+    edgeTo[v] = w;
+    distTo[v] = distTo[w] + 1;
+
+    for (Integer x : neighbours[v]) {
+      dfsMarker(x, v, marked, edgeTo, distTo);
+    }
+  }
+
+  /**
+   * Find the shortest path between vertex v and all other vertices in the graph.
+   *
+   * @param v
+   * @return Iterable of distances from vertex v
+   */
+  public Iterable<Integer> distances(int v) {
+    Integer[] edgeTo = new Integer[neighbours.length];
+    Integer[] distTo = new Integer[neighbours.length];
+
+    bfsEdger(v, edgeTo, distTo);
+
+    return Arrays.asList(distTo);
+  }
+
+  /**
+   * Find distances to other vertices in graph from one vertex.
+   *
+   * @param v      Vertex
+   * @param edgeTo Array of vertices which are the edge to the corresponding index
+   * @param distTo Array of distances from vertex v
+   */
+  private void bfsEdger(int v, Integer[] edgeTo, Integer[] distTo) {
+    LinkedList<Integer> q = new LinkedList<>();
+    q.add(v);
+    edgeTo[v] = -1;
+    distTo[v] = 0;
+
+    while (!q.isEmpty()) {
+      v = q.removeFirst();
+
+      for (int w : neighbours[v]) {
+        if (edgeTo[w] == null) {
+          edgeTo[w] = v;
+          distTo[w] = distTo[v] + 1;
+
+          q.add(w);
+        }
+      }
+    }
+  }
+
+  /**
    * String representation of the graph.
-   * 
+   *
    * @return String representation of the graph
    */
   public String toString() {
